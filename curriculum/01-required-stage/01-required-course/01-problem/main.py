4703621baa70e18c968679bb31f3e594c4bd9878
gkrtod4477 <dltngus98099@naver.com>
2026-03-18 23:43:49 +0900

    feat: rs01-rc01-p01 보너스 과제 작성

--- curriculum/01-required-stage/01-required-course/01-problem/main.py
+++ curriculum/01-required-stage/01-required-course/01-problem/main.py
@@ -1,11 +1,34 @@
 from pathlib import Path
+import csv
+from datetime import datetime
+
+
+def print_logs(title, logs):
+    print(title)
+    print("timestamp,event,message")
+    for log in logs:
+        print(f'{log["timestamp"]},{log["event"]},{log["message"]}')
+    print()
 
 
 def main():
     problem_dir = Path(__file__).resolve().parent
     log_file = problem_dir / "provided-files" / "logs" / "mission_computer_main.log"
 
-    print(log_file.read_text(encoding="utf-8"), end="")
+    with log_file.open(encoding="utf-8", newline="") as file:
+        logs = list(csv.DictReader(file))
+
+    reverse_logs = sorted(
+        logs,
+        key=lambda row: datetime.strptime(row["timestamp"], "%Y-%m-%d %H:%M:%S"),
+        reverse=True,
+    )
+
+    print_logs("=== 로그 출력: 과거 -> 현재 ===", logs)
+    print_logs(
+        "=== 로그 출력: 현재 -> 과거 ===",
+        reverse_logs,
+    )
 
 
 if __name__ == "__main__":
